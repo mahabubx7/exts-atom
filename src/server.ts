788d0cf6ac4db3a137049139e1c8690577aa66cb
Mahabub Alam <mahabubx7@gmail.com>
2023-06-06 21:39:32 +0600

    :hammer: Setup(server):server-route-hello-response

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
 import express, { Express } from "express";
+import { router } from "./router";
 
-const server: Express = express();
+const server: Express = express(); // express server-app instance
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(router); // route handling
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () =>
+  console.log(`🚀 EX-TS is running at http://localhost:${port}`)
+);
